e1d0a9982c3b46eb34074daae25ff46eeb36a797
Dalark <v_e_c_g@outlook.com>
2023-06-06 22:49:19 -0400

    inicios de la interfaz del juego

--- practicas/inputs_basicos.jl
+++ practicas/inputs_basicos.jl
@@ -1,4 +1,4 @@
-println("What is your name?")
+#= println("What is your name?")
 name = readline() # este genera un salto de linea
 println("Hello $name, nice too meet you!")
 
@@ -6,4 +6,12 @@ println("Hello $name, nice too meet you!")
 # sin salto de linea
 print("What is your lastname? ")
 last_name = chomp(readline())
-println("Hello $name $last_name, nice too meet you!")
+println("Hello $name $last_name, nice too meet you!") =#
+
+
+
+n = 0
+while n < 10
+    n += 1
+    println(n)
+end
